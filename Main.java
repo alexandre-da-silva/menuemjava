74e46b3bcb5633e2a5cfff843f88b5fb3ef99305
alexandre-da-silva <alexandresilvax899@gmail.com>
2026-03-09 20:04:17 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+import login.Painel;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Painel.menu();
+       //Painel.usuario();
+       //Painel.verificar();
+    }
+}
